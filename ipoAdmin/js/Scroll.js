2c030144d588b2830f1140943bea9246d6085278
KennethCandy <1041052286@qq.com>
2016-12-30 17:41:20 +0800

    2
    20161230

--- ipoAdmin/js/Scroll.js
+++ ipoAdmin/js/Scroll.js
@@ -1,5 +1,19 @@
-$('#scrollbody').scrollspy({ target: '#navbar-example' })  
+/*$('#scrollbody').scrollspy({ target: '#navbar-example' })  
 $('[data-spy="scroll"]').each(function () {
   var $spy = $(this).scrollspy('refresh')
   console.log(123456)
+  alert(13);
 }) 
+
+$(window).scroll(function () {  
+    $('#scrollbody').scrollspy({ target: '#navbar-example' })  
+    $('[data-spy="scroll"]').each(function () {
+      var $spy = $(this).scrollspy('refresh')  
+    })
+}); 
+$(function () {
+        $('.scrollspy-example').scrollspy({ target:'#navbar-example' });
+        alert(12);
+})
+*/
+$('#myModal').modal('toggle');
