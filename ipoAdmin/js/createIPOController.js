209832876ec0ba81aa0853a8276356c674117024
KennethCandy <1041052286@qq.com>
2017-01-04 17:58:30 +0800

    3

--- ipoAdmin/js/createIPOController.js
+++ ipoAdmin/js/createIPOController.js
@@ -1,6 +1,6 @@
 angular.module('ipoAdminApp.createIPOController', [])
 
-.controller('createIPOCtrl', [ '$scope', '$rootScope', '$sce', '$http', '$translate', 'sharedProperties', 'redirectService', 'applicationStatusService', 'timelineService', function($scope, $rootScope, $sce, $http, $translate, sharedProperties, redirectService, applicationStatusService, timelineService) {
+.controller('createIPOCtrl', [ '$scope', '$rootScope', '$sce', '$http', '$interval', '$translate', 'sharedProperties', 'redirectService', 'applicationStatusService', 'timelineService', function($scope, $rootScope, $sce, $http, $interval, $translate, sharedProperties, redirectService, applicationStatusService, timelineService) {
 	
 	$scope.getCurrentDate = function(value){
 		return getCurrentDate().substring(0, 4) +"/"+ getCurrentDate().substring(4, 6) +"/"+ getCurrentDate().substring(6);
@@ -13,6 +13,12 @@ angular.module('ipoAdminApp.createIPOController', [])
 	$scope.getTime = function(value){
 		return getTime(value);
 	}
+
+	$scope.theTime = new Date().toLocaleTimeString();
+
+    $interval(function () {
+        $scope.theTime = new Date().toLocaleTimeString();
+    }, 1000);
 	
 	
 	
